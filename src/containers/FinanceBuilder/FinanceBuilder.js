287f65832d1f63f28b6f49ab4b96662b38c3182a
Elza Mambetakunova <elza.mambetakunova@gmail.com>
2019-01-08 15:04:50 +0600

    added Item Component

---
+++ src/containers/FinanceBuilder/FinanceBuilder.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import Item from "../../components/ItemComponent/Item";
+
+
+class FinanceBuilder extends Component {
+
+    state = {
+
+    };
+
+    render() {
+
+        return (
+            <div>
+                <Item />
+            </div>
+        )
+    }
+}
+
+export default FinanceBuilder;
